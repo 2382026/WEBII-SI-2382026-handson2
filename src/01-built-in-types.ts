2a1c69de76feafd4d4396f7b72ac1359515ece75
2382026 <2382026@unai.edu>
2025-01-22 12:14:21 +0700

    Update

---
+++ src/01-built-in-types.ts
@@ -0,0 +1,13 @@
+let age: number = 25;
+let name: string = "John Doe";
+let isActive: boolean = true;
+let notFound: null = null;
+let notDefined: undefined = undefined;
+let anything: any = "Rose Valencia";
+anything = 30;
+
+let a = null;
+let b;
+console.log(b);
+
+export{};
